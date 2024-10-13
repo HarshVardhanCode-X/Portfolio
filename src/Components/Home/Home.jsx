c194fbfab183e7a2419f1314b135637f29c67c8a
Harsh Vardhan <harshvardhan232002@gmail.com>
2024-10-13 09:51:46 +0530

    second

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <>
       <section className='h-[100vh] bg-[#050505] w-full'>
-        <header className='bg-[#2D2D2D] px-2 w-full sticky inset-0 z-10'>
+        <header className='bg-[#2D2D2D] px-2 w-full sticky inset-0 z-10 h-[9vh]'>
           <div className='flex items-center justify-between px-5 py-4'>
 
             <div className='flex items-center justify-center text-white'>
@@ -28,7 +28,7 @@ function Home() {
           </div>
         </header>
 
-        <main className='h-[90vh]'>
+        <main className='h-[91vh]'>
           <div className='flex flex-column justify-center relative h-full'>
             <div className='flex items-center justify-center relative h-[70vh]'>
               <h1 className='text-[#303030] font-bold text-[200px]'>PORTFOLIO</h1>
